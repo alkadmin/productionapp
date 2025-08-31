0a6d7de654877dd922dbd71f99d3822714912ab1
alkadmin <admin@alkaliza.com>
2025-08-31 15:18:45 -0500

    chore: translate issue button

--- pages/tortillas/index.jsx
+++ pages/tortillas/index.jsx
@@ -3,15 +3,15 @@
 
 import React from 'react';
 import ProductionOrders from '../../components/ProductionOrderList/ProductionOrderList';
+import { useTranslation } from '../../utilities/i18n';
 
 const TortillaProductionOrders = () => {
- 
+  const { t } = useTranslation();
+
   return (
-    <div >
-   
-      <ProductionOrders type='Tortillas'/>
+    <div>
+      <ProductionOrders type={t('tortillas')} />
     </div>
-   
   );
 };
 
